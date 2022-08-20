54fdd8bf5fc0a6564f05cb4ae42b9ccbaefa118e
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-08-20 13:12:06 +0500

    Update Call-back-hell.js

--- Call-back-hell.js
+++ Call-back-hell.js
@@ -45,3 +45,37 @@
       }, 1000);
     }, 1000);
 
+// lets tru this by making a function and then call it.
+
+ const h1 = document.querySelector("h1");
+    h1.style.fontSize = "50px";
+    h1.style.textAlign = "center";
+    h1.style.color = "white";
+    h1.style.margin = "auto, auto";
+    const delayColor = (newColor, delay, doNext) => {
+      setTimeout(() => {
+        document.body.style.backgroundColor = newColor;
+        doNext();
+      }, delay);
+    };
+
+    delayColor('red',1000, () => {
+        h1.innerText = "Hello I'm red";
+        delayColor('green',1000, () => {
+            h1.innerText = "Hello I'm green";
+            delayColor('purple',1000, () => {
+                h1.innerText = "Hello I'm purple";
+                 delayColor('orange',1000, () =>{
+                    h1.innerText = "Hello I'm ornage";
+                    delayColor('indigo',1000, () => {
+                        h1.innerText = "Hello I'm Indigo"
+                        delayColor("White",1000, () => {
+                            h1.style.color = "black";
+                            h1.innerText = "👿 I'm Call back Hell..."
+                        })
+                    })
+                 })
+            })
+        })
+    })
+
