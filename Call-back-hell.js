14d00668bce23a0052ff9e9024bb2d31e35fa075
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-08-20 12:31:21 +0500

    Update Call-back-hell.js

--- Call-back-hell.js
+++ Call-back-hell.js
@@ -1,1 +1,21 @@
+// normal way to do this  
+   const h1 = document.querySelector('h1');
+    h1.style.fontSize = "50px";
+    h1.style.textAlign = "center";
+    h1.style.color = "white";
+    h1.style.margin = "auto, auto"
+    setTimeout(() => {
+      document.body.style.backgroundColor = "red";
+    }, 1000);
+
+    setTimeout(() => {
+      document.body.style.backgroundColor = "green";
+    }, 2000);
+
+    setTimeout(() => {
+      document.body.style.backgroundColor = "purple";
+    }, 3000);
+
+// now try nested callbacks.
+
 
