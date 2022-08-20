070137edab8f40b549fd14e374561260cf8225e7
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-08-20 15:50:15 +0500

    Update Demo:Fake-requests-using-callbacks.js

--- Demo:Fake-requests-using-callbacks.js
+++ Demo:Fake-requests-using-callbacks.js
@@ -1,1 +1,44 @@
+//lets practice how to make fake requets using call backs in javascript.
+// now we are practing the fake requets by using two methods 
 
+
+// the call backs 
+ const fakeRequests = (url, success, failure) => {
+            const delay = Math.floor(Math.random()*4500)+500;
+            setTimeout(() => {
+                 if(delay > 4000){
+                    failure(`Connection Timeout :(`);
+                 } else{
+                    success(`Here is your data requets ${url}`);
+                    
+                 }
+            },delay)
+
+        }
+
+        fakeRequests("books.com",
+        function(success){
+            console.log("IT WORKED");
+            console.log(success)
+        }, function(failure){
+            console.log("Opps Error")
+            console.log(failure)
+        }
+        
+        )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// the promises.
