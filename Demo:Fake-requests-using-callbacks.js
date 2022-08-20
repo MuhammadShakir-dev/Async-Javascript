df3cc9aed29278b238aa301cac36cebaa79a6ca8
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-08-20 16:01:00 +0500

    Update Demo:Fake-requests-using-callbacks.js

--- Demo:Fake-requests-using-callbacks.js
+++ Demo:Fake-requests-using-callbacks.js
@@ -47,5 +47,47 @@
         }
         
         )
+// make another one nested request.
+ const fakeRequests = (url, success, failure) => {
+            const delay = Math.floor(Math.random()*4500)+500;
+            setTimeout(() => {
+                 if(delay > 4000){
+                    failure(`Connection Timeout :(`);
+                 } else{
+                    success(`Here is your data requets ${url}`);
+                    
+                 }
+            },delay)
 
+        }
+
+        fakeRequests("books.com",
+        function(success){
+            console.log("IT WORKED");
+            console.log(success)
+            fakeRequests('books.com/page2',
+            function(success){
+                console.log("IT WORKED AGAIN");
+                  console.log(success)
+                  fakeRequests('books.com/page3',
+                  function(success){
+                  console.log("IT WORKED AGAIN");
+                  console.log(success)
+                  },
+                  function(failure){
+                    console.log("ERROR!!!", failure)
+                  }
+                  
+                  )
+            },
+            function(failure){
+                console.log("ERROR!!!", failure)
+            }
+            )
+        }, function(failure){
+            console.log("Opps Error")
+            console.log(failure)
+        }
+        
+        )
 // the promises.
